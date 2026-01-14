9cb97cefb5a59520db07163c0f6fb96cf84b5d63
JessePinkman <affangashanta@gmail.com>
2026-01-14 14:48:57 +0700

    menambahkan parameter dan argument

--- Program.cs
+++ Program.cs
@@ -1,28 +1,29 @@
-﻿int a = 10, b = 2, c = 0;
+﻿int c = 0;
 
-void Tambah()
+void Tambah(int a, int b)
 {
     c = a + b;
     Console.WriteLine($"{a} + {b} = {c}");
 }
 
-void Kurang()
+void Kurang(int a, int b)
 {
     c = a - b;
     Console.WriteLine($"{a} - {b} = {c}");
 }
-void Kali()
+void Kali(int a, int b)
 {
     c = a * b;
     Console.WriteLine($"{a} * {b} = {c}");
 }
-void Bagi()
+void Bagi(int a,int b)
 {
     c = a / b;
     Console.WriteLine($"{a} / {b} = {c}");
 }
 
-Tambah();
-Kurang();
-Kali();
-Bagi();
+Tambah(8, 2);
+Kurang(5, 2);
+Tambah(6, 7);
+Kali(6, 9);
+Bagi(8, 4);
