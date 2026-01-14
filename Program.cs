e6794fb798a7aa1397abbd17ba298cd5b353cc0f
JessePinkman <affangashanta@gmail.com>
2026-01-14 14:32:58 +0700

    menambahkan method tambah, kurang, kali, bagi

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 10, b = 2, c = 0;
+
+void Tambah()
+{
+    c = a + b;
+    Console.WriteLine($"{a} + {b} = {c}");
+}
+
+void Kurang()
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
+void Kali()
+{
+    c = a * b;
+    Console.WriteLine($"{a} * {b} = {c}");
+}
+void Bagi()
+{
+    c = a / b;
+    Console.WriteLine($"{a} / {b} = {c}");
+}
+
+Tambah();
+Kurang();
+Kali();
+Bagi();
